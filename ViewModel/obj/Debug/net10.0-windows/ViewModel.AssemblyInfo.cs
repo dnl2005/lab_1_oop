4634141c32122fa415e7552a9b75956e6f846360
dnl <danek2005w@gmail.com>
2026-01-28 12:06:25 +0300

    lib. ui patch

--- ViewModel/obj/Debug/net10.0-windows/ViewModel.AssemblyInfo.cs
+++ ViewModel/obj/Debug/net10.0-windows/ViewModel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8990d5b97ab374a3ba02c8d9526dccef33a2f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
