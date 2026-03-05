e6a9296e3ee8bde9dfed4dffec5bd1bb775147da
dnl <danek2005w@gmail.com>
2026-03-05 14:04:53 +0300

    toolkit

--- UI/obj/Debug/net10.0-windows/Views/TriggersView.g.i.cs
+++ UI/obj/Debug/net10.0-windows/Views/TriggersView.g.i.cs
@@ -53,7 +53,7 @@ namespace UI.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UI;component/views/triggersview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UI;V1.0.0.0;component/views/triggersview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\TriggersView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
